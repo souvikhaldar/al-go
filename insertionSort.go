7f1a0508ea73478461fc20e5e07845b22cefdcc1
souvik.haldar <souvik.haldar@imperva.com>
2022-11-03 20:08:56 +0530

    Add insertion sort

---
+++ insertionSort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func InsertionSort(inp []int) []int {
+	for i := 0; i < len(inp); i++ {
+		inter := inp[i:]
+		pos, min := minimum(inter)
+		inp[i], inp[pos+i] = min, inp[i]
+	}
+	return inp
+}
+func minimum(inp []int) (pos, val int) {
+	min := 999999
+	for p, i := range inp {
+		if i < min {
+			min = i
+			pos = p
+		}
+	}
+	return pos, min
+}
+
+func main() {
+	inp := []int{4, 6, 2, 8, 1, 9, 5, 4, 4, 76, 123, 87, 9, -4, -2, 34}
+	fmt.Println("Input: ", inp)
+	fmt.Println("After sorting: ", InsertionSort(inp))
+}
